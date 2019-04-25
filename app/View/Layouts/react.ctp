6048be24da005b3f32544ba32e840eb0d30d582f
Patrick Barabe <pbarabe@email.arizona.edu>
2019-04-25 11:50:20 -0700

    Implement injection of data from ajax/api call on page load

---
+++ app/View/Layouts/react.ctp
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <?php echo $this->Html->charset(); ?>
+
+  <title>React Integrated CakePHP Application</title>
+</head>
+
+<body>
+  <div id="app"></div>
+
+  <?php echo $this->Html->script('bundle'); ?>
+</body>
+</html>
--- app/src/components/App.js
+++ app/src/components/App.js
@@ -1,15 +1,58 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("pages/get_data")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            data: result.data
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
   }
 
   render() {
-    return (
-      <p>Hello World</p>
-    );
+    const { error, isLoaded, data } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div>
+          <p>{data.greeting}, {data.name}.</p>
+          <ul>
+          {data.items.map(item => (
+            <li key={item.name}>
+            {item.name} {item.price}
+            </li>
+          ))}
+          </ul>
+        </div>
+      )
+    }
   }
 }
 
-export default App;
+export default App
